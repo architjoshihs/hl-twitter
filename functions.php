c0eeba54fdec6037861ec56bebcd23f7cdde60e6
Dachande663 <Dachande663@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-06 08:57:25 +0000

    Updates w/ dial home
    git-svn-id: https://plugins.svn.wordpress.org/hl-twitter/trunk@285659 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions.php
+++ functions.php
@@ -143,6 +143,14 @@ function hl_twitter_install() {
 	wp_schedule_event(time(), 'hl_1hr', HL_TWITTER_SCHEDULED_EVENT_ACTION); # Add cron event handler
 	update_option(HL_TWITTER_UPDATE_FREQUENCY, 'hl_1hr');
 	
+	/* Plugin Dial Home
+	 * 
+	 * This is a completely anonymous remote call to the original developers server.
+	 * The only data tracked is an non-reversible hash of the site URL so duplicates
+	 * aren't recorded. It is so installations can be shown on the plugin website.
+	 */
+	wp_remote_get('http://hybridlogic.co.uk/wp-plugin-activation.php?plugin=hl_twitter&hash='.md5(get_bloginfo('url')));
+	
 	$sql = "
 		CREATE TABLE `".$table_prefix.HL_TWITTER_DB_PREFIX."replies` (
 			`id` int(11) NOT NULL AUTO_INCREMENT,
